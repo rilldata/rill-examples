7bfc45fd52656a14a610992198174c54b97e950f
Scott Cohen <82397988+cohenscottr@users.noreply.github.com>
2023-09-11 15:53:17 -0700

    Update auction_data_model.sql

--- rill-openrtb-prog-ads/models/auction_data_model.sql
+++ rill-openrtb-prog-ads/models/auction_data_model.sql
@@ -1,5 +1,2 @@
-select * EXCLUDE (device_region), 
-CASE WHEN device_region ILIKE '%/%' THEN SPLIT(device_region, '/')[2] ELSE 'Unknown' END AS device_state, 
-CASE WHEN device_region ILIKE '%/%' THEN SPLIT(device_region, '/')[1] ELSE 'Unknown' END AS device_country
-
+select * 
 from auction_data_raw
