68e745f4e8799321cfa6f7a50a9bd476a5ed6752
royendo <67675319+royendo@users.noreply.github.com>
2024-10-08 10:43:47 -0600

    Modifying row policies for embed example

---
+++ rill-openrtb-prog-ads/models/model.sql
@@ -0,0 +1,7 @@
+-- Model SQL
+-- Reference documentation: https://docs.rilldata.com/reference/project-files/models
+
+    SELECT * FROM (VALUES 
+      ('Disney', 'domain.com'),
+
+    ) AS t(PubName, domain)
