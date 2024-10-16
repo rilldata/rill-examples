50eac692204e84aa2744cf7eb6861393a394b184
royendo <67675319+royendo@users.noreply.github.com>
2024-10-16 15:30:26 -0600

    Delete model.sql

--- rill-openrtb-prog-ads/models/model.sql
+++
@@ -1,8 +0,0 @@
--- Model SQL
--- Reference documentation: https://docs.rilldata.com/reference/project-files/models
-
-    SELECT * FROM (VALUES 
-      ('Disney', 'test@domain.com'),
-      ('Disney', 'roy.endo@rilldata.com')
-
-    ) AS t(PubName, email)
