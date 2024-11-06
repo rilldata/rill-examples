0efe47403cb5b28e031ded82bcdf0ac8498f22fc
royendo <67675319+royendo@users.noreply.github.com>
2024-11-06 16:29:06 -0700

    testing embedding use-case

--- rill-openrtb-prog-ads/models/mapping.sql
+++ rill-openrtb-prog-ads/models/mapping.sql
@@ -2,8 +2,8 @@
 -- Reference documentation: https://docs.rilldata.com/reference/project-files/models
 
     SELECT * FROM (VALUES 
-      ('Disney', 'domain.com', ''),
-      ('Disney', 'rilldata.com', ''),
-      ('LG USA', 'rilldata.com', 'roy.endo@rilldata.com'),
-      ('Pluto', 'rilldata.com', '')
-    ) AS t(PubName, domain, email)
+      ('Disney', 'domain.com'),
+      ('Disney', 'rilldata.com'),
+      ('LG USA', 'partnercompany.com'),
+      ('Pluto', 'rilldata.com')
+    ) AS t(PubName, domain)
