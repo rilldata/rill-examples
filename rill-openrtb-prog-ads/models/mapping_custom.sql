022458a6ed695199e84e33a7cc8ff47ea0d92d05
royendo <67675319+royendo@users.noreply.github.com>
2024-12-17 14:27:33 -0700

    openrtb testing

--- rill-openrtb-prog-ads/models/mapping_custom.sql
+++ rill-openrtb-prog-ads/models/mapping_custom.sql
@@ -2,8 +2,7 @@
 -- Reference documentation: https://docs.rilldata.com/reference/project-files/models
 
     SELECT * FROM (VALUES 
-      ('Disney', 'Value0'),
-      ('Disney', 'Value2'),
+      ('Disney', 'Value1'),
       ('LG USA', 'Value2'),
-      ('Pluto', 'Value2')
+      ('Pluto', 'Value3')
     ) AS t(PubName, custom_attribute)
