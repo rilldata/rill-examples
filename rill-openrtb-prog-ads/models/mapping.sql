0ba87f8c201c4bb8ba0966b13c64130303a5bd8f
royendo <67675319+royendo@users.noreply.github.com>
2025-01-31 11:18:23 -0700

    simplifying open RTB project (will remake embed project in a new project)

--- rill-openrtb-prog-ads/models/mapping.sql
+++
@@ -1,9 +0,0 @@
--- Model SQL
--- Reference documentation: https://docs.rilldata.com/reference/project-files/models
-
-    SELECT * FROM (VALUES 
-      ('Disney', 'domain.com'),
-      ('Disney', 'rilldata.com'),
-      ('LG USA', 'partnercompany.com'),
-      ('Pluto', 'rilldata.com')
-    ) AS t(PubName, domain)
--- rill-openrtb-prog-ads/models/mapping_custom.sql
+++
@@ -1,8 +0,0 @@
--- Model SQL
--- Reference documentation: https://docs.rilldata.com/reference/project-files/models
-
-    SELECT * FROM (VALUES 
-      ('LG USA', 'Value1'),
-      ('Disney', 'Value2'),
-      ('Pluto', 'Value3')
-    ) AS t(PubName, custom_attribute)
