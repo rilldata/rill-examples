999255284396a55b3d9e1980fbeb96f608a77cc2
royendo <67675319+royendo@users.noreply.github.com>
2025-03-25 10:46:51 -0600

    Revert "Auto committed by Rill"
    This reverts commit 786e148b497cbb50d166b30c039a16efcdd48d8d.

--- my-rill-tutorial/2. Rill Advanced/models/advanced_commits___model.sql
+++ my-rill-tutorial/2. Rill Advanced/models/advanced_commits___model.sql
@@ -20,7 +20,6 @@ SELECT
     author_date,
     author_name,
     directory_path,
-    -- UNNEST(regexp_split_to_array(directory_path, '/')),
     filename,
     STRING_AGG(DISTINCT commit_msg, ', ') AS commit_msg,
 
