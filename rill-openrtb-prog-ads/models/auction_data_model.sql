99fd00633fb215a596ea9fdac454037d5edb372b
TheRillJon <jon.walls@rilldata.com>
2025-05-21 20:37:43 +0100

    Shift the __time column to give recent data

--- rill-openrtb-prog-ads/models/auction_data_model.sql
+++ rill-openrtb-prog-ads/models/auction_data_model.sql
@@ -2,7 +2,17 @@
 -- Reference documentation: https://docs.rilldata.com/reference/project-files/models
 -- @materialize: true
 
-select * EXCLUDE (device_region), 
-CASE WHEN device_region ILIKE '%/%' THEN SPLIT(device_region, '/')[2] ELSE 'Unknown' END AS device_state, 
-CASE WHEN device_region ILIKE '%/%' THEN SPLIT(device_region, '/')[1] ELSE 'Unknown' END AS device_country
-from auction_data_raw
+WITH time_shift AS (
+  SELECT 
+    MAX(__time) AS max_time
+  FROM auction_data_raw
+),
+shifted_data AS (
+  SELECT
+    __time + (CURRENT_TIMESTAMP - time_shift.max_time) AS __time,
+    * EXCLUDE (__time, device_region),
+    CASE WHEN device_region ILIKE '%/%' THEN SPLIT(device_region, '/')[2] ELSE 'Unknown' END AS device_state,
+    CASE WHEN device_region ILIKE '%/%' THEN SPLIT(device_region, '/')[1] ELSE 'Unknown' END AS device_country
+  FROM auction_data_raw, time_shift
+)
+SELECT * FROM shifted_data
--- rill-openrtb-prog-ads/models/bids_data_model.sql
+++ rill-openrtb-prog-ads/models/bids_data_model.sql
@@ -2,4 +2,15 @@
 -- Reference documentation: https://docs.rilldata.com/reference/project-files/models
 -- @materialize: true
 
-select * from bids_data_raw
+WITH time_shift AS (
+  SELECT 
+    MAX(__time) AS max_time
+  FROM bids_data_raw
+),
+shifted_data AS (
+  SELECT
+    __time + (CURRENT_TIMESTAMP - time_shift.max_time) AS __time,
+    * EXCLUDE (__time)
+  FROM bids_data_raw, time_shift
+)
+SELECT * FROM shifted_data
