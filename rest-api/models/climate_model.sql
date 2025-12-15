ebed23781cf80e50b7b9a31c2577f9ae52c48e0b
royendo <67675319+royendo@users.noreply.github.com>
2025-12-15 10:22:25 -0500

    rest-api and yt_analytics

--- rest-api/models/climate_model.sql
+++ rest-api/models/climate_model.sql
@@ -1,6 +1,6 @@
 -- Model SQL
 -- Reference documentation: https://docs.rilldata.com/build/models
-
+-- @materialze: true
 SELECT 
   
   aq.*,
@@ -17,7 +17,7 @@ SELECT
   temperature_2m,
   relativehumidity_2m
   
-  from top50cities_aq aq
+  from cities_aq aq
 
-left join top50cities_marine marine on (aq.city = marine.city and aq.time = marine.time)
-left join top50cities_temps temp on (aq.city = temp.city and aq.time = temp.time)
+left join cities_marine marine on (aq.city = marine.city and aq.time = marine.time)
+left join cities_temps temp on (aq.city = temp.city and aq.time = temp.time)
