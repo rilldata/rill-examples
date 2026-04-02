b7a4a0dd2456f19a1fd4dd1be9a8e4f05d27e92b
royendo <67675319+royendo@users.noreply.github.com>
2026-04-02 13:54:21 -0400

    clean up

--- rill-bluesky/models/jetstream_parquet (copy).sql
+++
@@ -1,5 +0,0 @@
--- @materialize: true
--- Model SQL
--- Reference documentation: https://docs.rilldata.com/reference/project-files/models
-
-select commit.record from jetstream_parquet
--- rill-mp-demo/models/search_amazon___search_amazon_model.sql
+++
@@ -1,4 +0,0 @@
--- Model SQL
--- Reference documentation: https://docs.rilldata.com/reference/project-files/models
-
-select * from search_amazon___search_amazon
